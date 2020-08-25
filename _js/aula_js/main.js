c4c540ee480d68da1f2a0c9bcd5b720e4e2fcf03
Willian Santos <49081567+ykandakota@users.noreply.github.com>
2020-08-25 14:09:02 -0300

    conceitos iniciais javascript

--- _js/aula_js/main.js
+++ _js/aula_js/main.js
@@ -2,8 +2,14 @@
 // atividade do bootcamp de gameDev DIO ago 2020//
 // author: willian santos //
 
+var msg = "hello ";
+var nome = "willian"
 
+alert(msg + nome);                                            // cria um alerta concatenando duas variaveis //
 
-alert("ola mundo");
+console.log(msg);                                             // escreve no console //
+console.log(nome.replace("willian", "joaquim"));              // alterna os valores de duas sub-strings //
+console.log(msg.toUpperCase());                               // imprime o conteúdo de uma string em caixa alta //
+
 
 
