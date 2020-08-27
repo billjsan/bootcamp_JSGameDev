8fe5f3c52b255f38fb82d0f162c5d0ab6fd7bcdf
Willian Santos <49081567+ykandakota@users.noreply.github.com>
2020-08-27 14:11:55 -0300

    estudo de conceitos basicos e funcionalidades javascript

---
+++ _js/aula_js/main2.js
@@ -0,0 +1,34 @@
+
+    //alert("olá");
+
+    var userstring;
+    var numero;
+    var msg;
+
+    //userstring = prompt("digite uma palavra");
+    //numero = parseInt(userstring);                               // transforma a string numerica em um dado do tipo numerico //
+    //console.log(numero, typeof numero); 
+    
+    //console.log(userstring.split('.'));                             // separa uma string em substrings separadas por . //
+    //msg = userstring.split('.');
+    //console.log(msg);
+    //console.log(msg[1]);
+
+
+    var frutas = ['manga','laranja', 'banana', 'limao', 'acerola', 'jabuticaba'];
+    var user;
+    var op;
+    var cesta;
+
+    console.log("lista de frutas: " + frutas);
+    //user = prompt("esolha a opcao:");
+    op = parseInt(user);
+    //console.log(op);
+    cesta = frutas.slice(0,5);                                     // escolhe itens de uma lista (inicio - fim) da selecao //
+    console.log("a fruta escolhida foi:" + cesta);
+
+
+
+
+   
+
